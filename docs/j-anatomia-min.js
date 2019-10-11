186b789e9ea470b53ee676f19b5c23c6a749a70a
marcinretecki <info@marcinretecki.com>
2019-10-11 21:20:14 +0200

    Example of new order logic is tested and works fine

--- docs/j-anatomia-min.js
+++ docs/j-anatomia-min.js
@@ -4,7 +4,7 @@
 (function() {
   'use strict';
 
-  var debug = false;
+  var debug = true;
 
   if ( ( location.hostname === '' ) || ( location.hostname === 'localhost' ) ) {
     debug = true;
@@ -15,53 +15,21 @@
 
   //  Elements
   var elements = {
-    sectionBag: document.getElementById('section-bag'),
-    bagItems: document.getElementById('bag-items'),
-    bagSum: document.getElementById('bag-sum'),
-    bagLink: document.getElementById('bag-link'),
+    orderCount: document.getElementById('order-count'),
+    orderMinus: document.getElementById('order-minus'),
+    orderPlus: document.getElementById('order-plus'),
+    orderLink: document.getElementById('order-link'),
   };
 
-  var bundleCountSum = 0;
-  var orderSum = 0;
-  var bagString = '';
-
-  var products = {};
-
-  products['AN-1'] = {
-    productId: 'AN-1',
-    title: 'Anatomia norweskiego',
-    bundle: 'książka',
-    bundleCount: 1,
-    // price: 299,
-    price: 399,
-  };
-
-  products['AN-5'] = {
-    productId: 'AN-5',
-    title: 'Anatomia norweskiego',
-    bundle: '5 książek + 1 sztuka gratis',
-    bundleCount: 6,
-    // price: 1495,
-    price: 1995,
-  };
-
-  products['AN-12'] = {
-    productId: 'AN-12',
-    title: 'Anatomia norweskiego',
-    bundle: '12 książek + 3 sztuki gratis',
-    bundleCount: 15,
-    // price: 3585,
-    price: 4785,
-  };
-
-  var bag = [];
-  var bagElements = [];
+  var orderCount = 1;
+  var orderSum = 399;
 
   //  init
   function init() {
     showDebugMsg('init');
 
     addEventListeners();
+    listenToOrderCount();
   }
 
   //  add listeners
@@ -73,20 +41,15 @@
   function clickHandler(event) {
     var target;
 
-    //  get js-order button click
-    target = getClosest(event.target, '.js-order');
+    //  get remove-item button
+    target = getClosest(event.target, '.js-plus');
 
-    //  if target is not a js-order, try remove-item button
+    //  if target is not a js-plus, try remove-item button
     if (target === null) {
-      target = getClosest(event.target, '.js-remove-item');
+      target = getClosest(event.target, '.js-minus');
     }
 
-    //  if target is not a js-remove-item, try js-close-bag
-    if (target === null) {
-      target = getClosest(event.target, '.js-close-bag');
-    }
-
-    //  if target is not a js-remove-item, try js-slider
+    //  if target is not a js-minus, try js-slider
     if (target === null) {
       target = getClosest(event.target, '.js-slider');
     }
@@ -103,23 +66,18 @@
 
     showDebugMsg(target);
 
-    //  Add item to bag
-    if (target.classList.contains('js-order')) {
-      addItemToBag(target);
+    //  add one
+    if (target.classList.contains('js-plus')) {
+      addOrderCount(target);
       return;
     }
 
-    //  remove item from bag
-    if (target.classList.contains('js-remove-item')) {
-      removeItemFromBag(target);
+    //  substract one
+    if (target.classList.contains('js-minus')) {
+      substractOrderCount(target);
       return;
     }
 
-    //  close bag
-    //  dunno if needed
-    if (target.classList.contains('js-close-bag')) {
-      return;
-    }
 
     //  Slider
     if (target.classList.contains('js-slider')) {
@@ -140,155 +98,101 @@
     }
   }
 
-  //  add item to bag
-  function addItemToBag(target) {
-    showDebugMsg('add item to bag');
 
-    //  get product id
-    var productId = target.getAttribute('data-ns-product-id');
-
-    //  no id
-    if (!productId) {
-      showDebugMsg('product has no id');
-      return;
-    }
-
-    //  get product info
-    var product = products[productId];
-    var newBagElement = createBagItem(product);
-
-    //  add item to bag
-    bag.push(product);
-
-    //  add element referance to bagElements
-    bagElements.push(newBagElement);
-
-    showDebugMsg('Bag:');
-    showDebugMsg(bag);
-
-    updateBag();
-
-    //  first item in the bag
-    if (bag.length === 1) {
-      appendElement(newBagElement, elements.bagItems, true);
-      showBag();
-    }
-    else {
-      appendElement(newBagElement, elements.bagItems, false);
-    }
+  function addOrderCount() {
+    orderCount = parseInt(orderCount) + 1;
+    updateOrder();
   }
 
+  function substractOrderCount() {
+    orderCount = parseInt(orderCount) - 1;
+    updateOrder();
 
-  //  create bag item
-  //  @return created element
-  function createBagItem(product) {
-    var productEl = document.createElement('div');
-    var titleEl = document.createElement('span');
-    var titleCiteEl = document.createElement('cite');
-    var br = document.createElement('br');
-    var bundleEl = document.createElement('span');
-    var priceEl = document.createElement('i');
-
-    productEl.className = 'bag-item js-remove-item';
-    titleEl.className = 'bag-item__title';
-    priceEl.className = 'bag-item__price';
-
-    titleCiteEl.innerHTML = product.title;
-    bundleEl.innerHTML = product.bundle;
-    priceEl.innerHTML = product.price + ' kr';
-
-    titleEl.appendChild(titleCiteEl);
-    titleEl.appendChild(br);
-    titleEl.appendChild(bundleEl);
-
-    productEl.appendChild(titleEl);
-    productEl.appendChild(priceEl);
-
-    return productEl;
-  }
-
-  //  Remove
-  function removeItemFromBag(target) {
-    showDebugMsg('Remove item from bag');
-    showDebugMsg(target);
-
-    bag.forEach(function(product, index) {
-      if (bagElements[index] === target) {
-        //  remove element from bagElements
-        bagElements.splice(index, 1);
-
-        //  remove product from bag
-        bag.splice(index, 1);
-
-        //  remove from html
-        removeElement(target);
-
-        showDebugMsg('product index: ' + index);
-      }
-    });
-
-    updateBag();
-
-    //  nothing in the bag
-    if (bag.length === 0) {
-      hideBag();
-    }
-
-    showDebugMsg(bag);
   }
 
 
 
-  function updateBag() {
-    updateBundleCountSum();
+  //  Update order
+  function updateOrder() {
+    checkOrderCount();
     updateOrderSum();
-    updateBagString();
+    updateOrderString();
+    updateOrderCount();
   }
 
-  //  update bundleCountSum
-  function updateBundleCountSum() {
-    var newBundleCountSum = 0;
 
-    bag.forEach(function(product) {
-      newBundleCountSum += product.bundleCount;
-    });
+  //  check if order is not below 1
+  function checkOrderCount() {
+    showDebugMsg("check order count");
+    if (orderCount < 1) {
+      showDebugMsg("order count corrected");
+      orderCount = 1;
+    }
+  }
 
-    bundleCountSum = newBundleCountSum;
 
-    showDebugMsg('bundleCountSum: ' + bundleCountSum);
+  function updateOrderCountFromField() {
+    if (elements.orderCount.value !== '' && parseInt(elements.orderCount.value) !== orderCount) {
+      showDebugMsg('update order count from field');
+      orderCount = elements.orderCount.value;
+      updateOrder();
+    }
+  }
+
+
+
+  //  update order count
+  function updateOrderCount() {
+    showDebugMsg("check order count field");
+    elements.orderCount.value = orderCount;
   }
 
   //  update order sum
   function updateOrderSum() {
     var newSum = 0;
 
-    bag.forEach(function(product) {
-      newSum += product.price;
-    });
+    if (orderCount < 6) {
+      newSum = orderCount * 399;
+    }
+    else if (orderCount < 11) {
+      newSum = orderCount * 349;
+    }
+    else if (orderCount < 31) {
+      newSum = orderCount * 319;
+    }
+    else if (orderCount > 30) {
+      newSum = orderCount * 299;
+    }
 
     orderSum = newSum;
-
-    //  show new sum
-    elements.bagSum.innerHTML = orderSum;
 
     showDebugMsg('orderSum: ' + orderSum);
   }
 
-  //  update bag string
-  function updateBagString() {
-    var newBagString = '';
+  //  update order string
+  function updateOrderString() {
 
-    bag.forEach(function(product) {
-      newBagString += product.productId + '|';
-    });
-
-    bagString = newBagString;
-
-    showDebugMsg('newBagString: ' + newBagString);
+    showDebugMsg('newOrderString: ' + orderCount);
 
     //  update link
-    elements.bagLink.href = kassaUrl + '?bag=' + bagString;
+    elements.orderLink.href = kassaUrl + '?bagx=' + orderCount;
   }
+
+  //
+  //  Observe Order count field
+  //
+  function listenToOrderCount() {
+
+    elements.orderCount.addEventListener(
+      'input',
+      function(event) {
+        updateOrderCountFromField();
+      },
+      false
+    );
+
+  }
+
 
 
   //  Move slider to next or prev slide
@@ -384,62 +288,6 @@
     });
   }
 
-  //  show bag
-  function showBag() {
-    showDebugMsg('show bag');
-
-    Velocity(
-      elements.sectionBag,
-      { opacity: [1, 0], translateY: [0, '-2rem'] },
-      { duration: 400, easing: 'easeInOut', display: 'block' }
-    );
-  }
-
-  //  hide bag
-  function hideBag() {
-    showDebugMsg('hide bag');
-
-    Velocity(
-      elements.sectionBag,
-      { opacity: 0, translateY: '-2rem' },
-      { duration: 400, easing: 'easeInOut', display: 'none' }
-    );
-  }
-
-  //  add element by slide down
-  function appendElement(el, parent, now) {
-
-    var duration;
-
-    if ( now ) {
-      duration = 0;
-    }
-    else {
-      duration = 400
-    }
-
-    Velocity(el, 'slideDown', {
-      duration: duration,
-      easing: 'easeInOut',
-      begin: function() {
-        parent.appendChild(el);
-      },
-    });
-
-  }
-
-  //  remove element by slide up
-  function removeElement(el) {
-    Velocity(el, 'slideUp', {
-      duration: 400,
-      easing: 'easeInOut',
-      complete: function() {
-        //  remove child from HTML
-        el.parentNode.removeChild(el);
-      },
-    });
-
-  }
 
   //  Init
   window.addEventListener(
